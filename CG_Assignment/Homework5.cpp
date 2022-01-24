d78c8d68b9cd434310414ea0c58f05277ddb0dd9
나은 송 <ene607@naver.com>
2022-01-25 02:12:40 +0900

    Homework5 추가

---
+++ CG_Assignment/Homework5.cpp
@@ -0,0 +1,201 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+void myMouse(int button, int state, int x, int y);
+
+using namespace std;
+
+GLuint programID;
+GLint screenX, screenY;
+GLfloat ndcX, ndcY;
+
+GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
+{
+	//create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+	//Read the vertex shader code from the file
+	string VertexShaderCode;
+	ifstream VertexShaderStream(vertex_file_path, ios::in);
+	if (VertexShaderStream.is_open())
+	{
+		string Line = "";
+		while (getline(VertexShaderStream, Line))
+			VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+
+	//Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertex_file_path);
+	char const* VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+	glCompileShader(VertexShaderID);
+
+	//Check Vertex Shader
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	vector<char> VertexShaderErrorMessage(InfoLogLength);
+	glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, VertexShaderErrorMessage.data());
+	fprintf(stdout, "%s\n", VertexShaderErrorMessage.data());
+
+	//Read the fragment shader code from the file
+	string FragmentShaderCode;
+	ifstream FragmentShaderStream(fragment_file_path, ios::in);
+	if (FragmentShaderStream.is_open())
+	{
+		string Line = "";
+		while (getline(FragmentShaderStream, Line))
+			FragmentShaderCode += "\n" + Line;
+		FragmentShaderStream.close();
+	}
+
+	//Compile Fragment Shader
+	printf("Compiling shader : %s\n", fragment_file_path);
+	char const* FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+	glCompileShader(FragmentShaderID);
+
+	//Check Fragment Shader
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	vector<char> FragmentShaderErrorMessage(InfoLogLength);
+	glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, FragmentShaderErrorMessage.data());
+	fprintf(stdout, "%s\n", FragmentShaderErrorMessage.data());
+
+	//Link the program
+	fprintf(stdout, "Linking program\n");
+	GLuint ProgramID = glCreateProgram();
+	glAttachShader(ProgramID, VertexShaderID);
+	glAttachShader(ProgramID, FragmentShaderID);
+	glLinkProgram(ProgramID);
+
+	// Check the program
+	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	vector<char> ProgramErrorMessage(max(InfoLogLength, int(1)));
+	glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, ProgramErrorMessage.data());
+	fprintf(stdout, "%s\n", ProgramErrorMessage.data());
+
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+
+	return ProgramID;
+}
+
+
+void renderScene(void)
+{
+	glutMouseFunc(myMouse);
+
+	GLuint Buffers[1];	//버퍼 아이디를 저장할 배열
+
+	GLfloat vertices[2][3] = { {ndcX, ndcY, 0.0},{0.0, 0.0, 0.0} };	//그리고자 하는 좌표를 저장한 배열
+
+	glGenBuffers(1, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	GLint posAttrib = glGetAttribLocation(programID, "vtxPosition");
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, ((void*)(0)));
+	glEnableVertexAttribArray(posAttrib);
+
+
+
+	//Clear all pixels
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Let's draw something here
+
+	//define the size of point and draw a point.
+	glDrawArrays(GL_LINES, 0, 2);
+
+	//Double buffer
+	glutSwapBuffers();
+}
+
+
+void init()
+{
+	//initilize the glew and check the errors.
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s' \n", glewGetErrorString(res));
+	}
+
+	//select the background color
+	glClearColor(0.373, 0.0, 1.0, 1.0);
+	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+
+
+
+
+}
+
+
+void myMouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		screenX = x;
+		screenY = y;
+		glutPostRedisplay();
+		printf("Screen Coordinate: (%d, %d)\n", x, y);
+
+		ndcX = (screenX / 0.5) / 479 - 1.0;
+		ndcY = -((screenY / 0.5) / 479 - 1.0);
+		printf("Normalized Coordinate: (%f, %f)\n\n", ndcX, ndcY);
+	}
+
+}
+
+
+int main(int argc, char** argv)
+{
+	//init GLUT and create Window
+	//initialize the GLUT
+	glutInit(&argc, argv);
+	//GLUT_DOUBLE enables double buffering (drawing to a background buffer while the other buffer is displayed)
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	//These two functions are used to define the position and size of the window. 
+	glutInitWindowPosition(200, 200);
+	glutInitWindowSize(480, 480);
+	//This is used to define the name of the window.
+	glutCreateWindow("작품명: 별똥별");
+
+	//call initization function
+	init();
+
+	//1.
+	//Generate VAO
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	//3. 
+	programID = LoadShaders("VertexShader5.txt", "FragmentShader5.txt");
+
+
+	glUseProgram(programID);
+
+
+	glutDisplayFunc(renderScene);
+
+	//enter GLUT event processing cycle
+	glutMainLoop();
+
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	return 1;
+}
